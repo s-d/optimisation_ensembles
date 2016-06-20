54d265cd2dd50aa305fb01671d92853d30390bca
Sam Dixon <samdixon05@gmail.com>
2016-06-20 14:40:43 +0100

    Commented python scripts.

--- PythonScripts/src/Consolidate.py
+++ PythonScripts/src/Consolidate.py
@@ -1,25 +1,32 @@
+# a script to consolidate important data from multiple .xlsx files.
 import os
 from openpyxl import Workbook
 from openpyxl import load_workbook
 import statistics
 
-cwb = Workbook()
-cws = cwb.active
-cws.title = 'Sheet1'
+# create workbook for results
+conWb = Workbook()
+conWs = conWb.active
+conWs.title = 'Sheet1'
 
 headers = ['ensemble id', 'number of runs', 'median fitness', 'mode fitness', 'mean fitness', 'lowest fitness',
            'highest fitness', 'range', 'algorithms']
 
-cws.append(headers)
+conWs.append(headers)
 
 rowCount = 2
+
+# for all excel files in current directory
 for excelFileName in os.listdir(os.getcwd()):
     if not excelFileName.endswith('.xlsx'):
         continue
 
     print("Adding " + excelFileName + " to file.")
+    # load workbook
     wb = load_workbook(filename=excelFileName)
     ws = wb.active
+
+    # pull data from file
     enId = ws['F2'].value
     runs = ws.max_row - 1
 
@@ -30,19 +37,21 @@ for excelFileName in os.listdir(os.getcwd()):
 
     fitness.sort()
 
-    cws['A' + str(rowCount)] = enId
-    cws['B' + str(rowCount)] = runs
-    cws['C' + str(rowCount)] = statistics.median(fitness)
+    # add pulled data to final workbook
+    conWs['A' + str(rowCount)] = enId
+    conWs['B' + str(rowCount)] = runs
+    conWs['C' + str(rowCount)] = statistics.median(fitness)
 
     try:
-        cws['D' + str(rowCount)] = statistics.mode(fitness)
+        conWs['D' + str(rowCount)] = statistics.mode(fitness)
     except statistics.StatisticsError:
         print('')
-    cws['E' + str(rowCount)] = statistics.mean(fitness)
-    cws['F' + str(rowCount)] = fitness[0]
-    cws['G' + str(rowCount)] = fitness[-1]
-    cws['H' + str(rowCount)] = fitness[-1] - fitness[0]
-    cws['I' + str(rowCount)] = ws['I2'].value
+    conWs['E' + str(rowCount)] = statistics.mean(fitness)
+    conWs['F' + str(rowCount)] = fitness[0]
+    conWs['G' + str(rowCount)] = fitness[-1]
+    conWs['H' + str(rowCount)] = fitness[-1] - fitness[0]
+    conWs['I' + str(rowCount)] = ws['I2'].value
 
     rowCount += 1
-cwb.save(filename='ensembleConsolidation.xlsx')
+# save workbook
+conWb.save(filename='ensembleConsolidation.xlsx')
