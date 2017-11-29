f7ecc0a6bd23b5932a13248172bee94687d64735
neaop <neaop@users.noreply.github.com>
2017-11-29 11:29:32 +0000

    Added additional unit tests.

--- src/test/java/solver/TestArgParser.java
+++ src/test/java/solver/TestArgParser.java
@@ -4,13 +4,24 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
-import java.util.Map;
-
-
 public class TestArgParser {
     @Rule
     public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
+    @Test
+    public void testValidArgs() {
+        String[] args = {"alg", "bin", "1"};
+        ArgParser.parseArgs(args);
+
+    }
+
+    @Test
+    public void testArgsWrongOrder() {
+        String[] args = {"bin", "alg", "1"};
+        exit.expectSystemExitWithStatus(1);
+        ArgParser.parseArgs(args);
+
+    }
 
     @Test
     public void testShortHelpArg() {
@@ -18,14 +29,16 @@ public class TestArgParser {
         exit.expectSystemExitWithStatus(1);
         ArgParser.parseArgs(args);
     }
+
     @Test
     public void testLongHelpArg() {
         String[] args = {"--help"};
         exit.expectSystemExitWithStatus(1);
         ArgParser.parseArgs(args);
     }
+
     @Test
-    public void testNoArgs(){
+    public void testNoArgs() {
         String[] args = {};
         exit.expectSystemExitWithStatus(1);
         ArgParser.parseArgs(args);
